2c30d8e548e94733522a8af94bed1afafda1ae6b
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2016-03-09 22:06:37 +0100

    icons of weather

--- app/app.component.js
+++ app/app.component.js
@@ -2,11 +2,19 @@
   app.AppComponent =
     ng.core.Component({
       selector: 'my-app',
-      template: '<h1>My First Angular 2 App</h1>'
+      template: '<div><img src="img/temp.png" /> <span>{{temperature}}</span> </div> \
+      <div><img src="img/press.png" /> <span>{{pressure}}</span>   </div> \
+      <div> <img src="img/humi.png" /> <span>{{humidility}}</span>  </div> \
+      <div><img src="img/wind.png" /> <span>{{wind}}</span> </div> \
+      <div><img src="img/sun.png" /> <span>{{sun}}</span> </div>'
     })
     .Class({
-      constructor: function() {}
+      constructor: function() {
+        this.temperature = '23';
+        this.pressure = 1002;
+        this.humidility = 80;
+        this.wind = 5;
+        this.sun = 4;
+      }
     });
 })(window.app || (window.app = {}));
-
-

ee7c44f18a9ef53ed5b5eb8fd93c674b528a92d5
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2016-03-10 16:17:57 +0100

    change images

--- app/app.component.js
+++ app/app.component.js
@@ -2,19 +2,22 @@
   app.AppComponent =
     ng.core.Component({
       selector: 'my-app',
-      template: '<div><img src="img/temp.png" /> <span>{{temperature}}</span> </div> \
-      <div><img src="img/press.png" /> <span>{{pressure}}</span>   </div> \
-      <div> <img src="img/humi.png" /> <span>{{humidility}}</span>  </div> \
-      <div><img src="img/wind.png" /> <span>{{wind}}</span> </div> \
-      <div><img src="img/sun.png" /> <span>{{sun}}</span> </div>'
+      template: '<div><img src="img/temp.png" /> <span>{{temperature}} °C</span> </div> \
+      <div><img src="img/press.png" /> <span>{{pressure}} hPa</span>   </div> \
+      <div> <img src="img/humi.png" /> <span>{{humidility}} %</span>  </div> \
+      <div><img src="img/wind.png" /> <span>{{wind}} m/s</span> </div> \
+      <div><img src="img/sun.png" /> <span>{{sun}} lum</span> </div>'
     })
     .Class({
       constructor: function() {
-        this.temperature = '23';
+        this.temperature = 18;
         this.pressure = 1002;
         this.humidility = 80;
         this.wind = 5;
         this.sun = 4;
+      },
+      fun : function() {
+        
       }
     });
 })(window.app || (window.app = {}));
